26f146c6d91af8c7a678c56aedc802d6557cf9a4
Deivid Almeida <61792159+Deividev365@users.noreply.github.com>
2020-06-23 16:53:18 -0300

    Arquivos relacionados a aula 2 de 5

--- README.md
+++ README.md
@@ -1,19 +1,12 @@
-# Part 1 to 5 completed :ballot_box_with_check:	
+# Part 2 to 5 completed :ballot_box_with_check:	
 
 # ImersaoGameDev  :video_game: 
 	
-
-##  This is the background image that i used as a scenary for my game :warning:	
-![cenario](https://user-images.githubusercontent.com/61792159/85351559-76f72500-b4da-11ea-9f1f-f2261799ccd2.png)
 
 
  # Story about the game :warning:	
  
  
-## this is the 'witch'
-![sdsdds](https://user-images.githubusercontent.com/61792159/85352386-34cee300-b4dc-11ea-8110-197d42553882.png)
 
-
-![localhost_5500 (3)](https://user-images.githubusercontent.com/61792159/85352223-d0138880-b4db-11ea-8db9-ae796832c22b.gif)
 
 Thanks, [Alura](https://www.alura.com.br/) :heart:
--- sketch.js
+++ sketch.js
@@ -1,29 +1,114 @@
 let imagemCenario;
 let imagemPersonagem;
+let imagemInimigo;
+let imagemGameOver;
+
+let somDoPulo;
 let cenario;
 let somDoJogo;
+let somMorreu;
+
 let personagem;
+let inimigo;
+
+const matrizInimigo = [
+  [0, 0],
+  [104, 0],
+  [208, 0],
+  [312, 0],
+  [0, 104],
+  [104, 104],
+  [208, 104],
+  [312, 104],
+  [0, 208],
+  [104, 208],
+  [208, 208],
+  [312, 208],
+  [0, 312],
+  [104, 312],
+  [208, 312],
+  [312, 312],
+  [0, 418],
+  [104, 418],
+  [208, 418],
+  [312, 418],
+  [0, 522],
+  [104, 522],
+  [208, 522],
+  [312, 522],
+  [0, 626],
+  [104, 626],
+  [208, 626],
+  [312, 626],
+]
+
+const matrizPersonagem =[
+  [0,0],
+  [220,0],
+  [440,0],
+  [660,0],
+  [0,270],
+  [220,270],
+  [440,270],
+  [660,270],
+  [0,540],
+  [220,540],
+  [440,540],
+  [660,540],
+  [0,810],
+  [220,810],
+  [440,810],
+  [660,810]
+]
 
 function preload(){
-  imagemCenario = loadImage('./assets/cenario/bckground.jpg'); 
-  imagemPersonagem = loadImage('./assets/personagem/man.jpg');  
+  imagemCenario = loadImage('./assets/cenario/sunset.jpg'); 
+  imagemPersonagem = loadImage('./assets/personagem/correndo.png');
+  imagemInimigo = loadImage('./assets/inimigos/gotinha.png');  
+  imagemGameOver = loadImage('./assets/gameOver/gameOver.png');
+  
   somDoJogo = loadSound('./assets/sons/bckGroundSong.mp3');
+  somDoPulo = loadSound('./assets/sons/somPulo.mp3');
+  somMorreu = loadSound('./assets/sons/somMorreu.mp3');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(imagemPersonagem);
+  personagem = new Personagem(matrizPersonagem, imagemPersonagem, 0, 110, 135, 220, 270);
+  inimigo = new Inimigo(matrizInimigo, imagemInimigo, width - 52, 52, 52, 104, 104);
   frameRate(40);
-  somDoJogo.loop();
+  somDoJogo.loop (); // change it after finish to 'loop()'
 
+}
+// evento no teclado
+function keyPressed() {
+  if(key === 'ArrowUp') {
+    personagem.pula();
+    somDoPulo.play();
+  }
 }
 
 function draw() {
   cenario.exibe();
   cenario.move();
-  
+
   personagem.exibe();
+  personagem.aplicaGravidade();
+  inimigo.exibe();
+  inimigo.move();
+
+  if(personagem.estaColidindo(inimigo)) {
+    console.log('colidiu');
+    somDoJogo.stop();
+    image(imagemGameOver, 0, 0, width, height);
+    somMorreu.play();
+    noLoop();
+    
+  }
+  
+
 }
+
 
 
